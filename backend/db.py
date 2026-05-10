75ccd59254f8c3e65d0ac41ab804067d9eff8b4a
Madumitha Saravana <madusaravana21@gmail.com>
2026-05-10 21:50:11 +0530

    Updated DB config

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,12 @@
+import os
 import psycopg2
 
 conn = psycopg2.connect(
-    host="db.rdsukgjfqdobiltdbasn.supabase.co",
-    database="postgres",
-    user="postgres",
-    password="Pythonbot@21042026",
-    port=5432
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT")
 )
 
 cursor = conn.cursor()
